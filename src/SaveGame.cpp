9305ad7816fe083dd0c6f7689679f603e488095e
hoanganhtu0922 <hoanganhtu0922@gmail.com>
2025-11-03 19:48:34 +0700

    add Save Game mode

---
+++ src/SaveGame.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+#include "Board.h"
+
+void SaveGame(const Board& board, std::string filename) {
+    std::ofstream out(filename);
+    if (!out.is_open()) return;
+
+    out << board.turn << std::endl;
+    out << board.black << std::endl;
+    out << board.points.size() << std::endl;
+
+    for (auto p : board.points) {
+        out << p.x << " " << p.y << " " << p.black << std::endl;
+    }
+
+    out.close();
+}
