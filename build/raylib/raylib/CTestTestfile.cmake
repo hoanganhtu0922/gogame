585268fe7be4902277941dbfac3196734e9f982a
hoanganhtu0922 <hoanganhtu0922@gmail.com>
2025-11-24 18:35:36 +0700

    complete

--- build/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/HOANG ANH/minifolder/cs160/Project/raylib/src
-# Build directory: C:/Users/HOANG ANH/minifolder/cs160/Project/build/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
